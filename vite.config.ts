6fea6a3db748504f700da8fbfed700bf36d1dc17
Andrii Zaluha <andrii.zaluha@comeet.co>
2026-02-11 18:51:51 +0200

    Revert "fixes"
    This reverts commit f60c0dfab13be14dc7cc5d8e682ed00036c66500.

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,6 @@ const pdfjsDistPath = path.dirname(require.resolve('pdfjs-dist/package.json'));
 const cMapsDir = normalizePath(path.join(pdfjsDistPath, 'cmaps'));
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/multisystem/' : '/',
   plugins: [
     react(),
     svgr(),
